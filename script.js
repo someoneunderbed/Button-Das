34de9c433846af16d9fd0cefd7f570d75ae63c09
someoneunderbed <booojglhj@gmail.com>
2021-07-24 22:14:13 +0400

    https://github.com/someoneunderbed/lesson

---
+++ script.js
@@ -0,0 +1,33 @@
+let parag = document.querySelector('p');
+
+
+let texty = document.querySelector('h1');
+
+
+let button = document.querySelector('button');
+
+
+
+// function Sstil(family, style, weight, size, height, align, color) {
+
+//     parag.style.fontFamily = '$(family)';
+//     parag.style.style = '$(style)';
+//     parag.style.fontWeight = '$(weight)';
+//     parag.style.fontSize = '$(size)';
+//     parag.style.lineHeight = '$(height)';
+//     parag.style.textAlign = '$(align)';
+//     parag.style.textAlign = '$(align)';
+//     parag.style.color = '$(color)';
+// }
+
+// Sstil("", "", "200", "40px", "140%", "center", "#333333");
+
+
+button.setAttribute('class', 'only-1')
+
+
+function clickImg() {
+    nav.classList.add("imageblock-1");
+}
+clickImg();
+
